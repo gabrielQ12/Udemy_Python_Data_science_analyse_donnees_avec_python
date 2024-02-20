757d044fe9531d7f358606500c379c5ec515b535
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-20 15:43:22 +0100

    conso pour chaque pays : DONE

--- numpy/Data_Science_numpy.py
+++ numpy/Data_Science_numpy.py
@@ -1,25 +1,26 @@
 import numpy as np
 
-world_alcohol = np.genfromtxt('world_alcohol.csv', delimiter=',',dtype="U75", skip_header=1) 
+world_alcohol = np.genfromtxt('world_alcohol.csv', delimiter=',', dtype="U75", skip_header=1)
 
-# print(world_alcohol[world_alcohol[:,2] == "Canada"])
+totals = {}
+is_year = world_alcohol[:, 0] == "1989"
+year = world_alcohol[is_year, :]
+countries = world_alcohol[:, 2]
 
-is_canada_1986 = (world_alcohol[:,2] == "Canada") & (world_alcohol[:,0] == "1986")
-canada_1986 = world_alcohol[is_canada_1986, :]
+for country in countries:
+    is_country = year[:, 2] == country
+    country_consumption = year[is_country, :]
+    alcohol_column = country_consumption[:, 4]
+    is_empty = alcohol_column == ''
+    alcohol_column[is_empty] = '0'
+    alcohol_column = alcohol_column.astype(float)
+    totals[country] = np.sum(alcohol_column)
+    
+# ici la boucle for est utilisée pour parcourir les éléments de la liste countries
+# pour chaque élément de la liste, on vérifie si la valeur de la colonne 2 de l'array year est égale à l'élément de la liste
+# si c'est le cas, on extrait les lignes correspondantes et on calcule la somme des valeurs de la colonne 4
+# on stocke le résultat dans le dictionnaire totals
+# on utilise la méthode astype() pour convertir le type de données de l'array alcohol_column en float
+# on utilise la méthode sum() pour calculer la somme des valeurs de l'array alcohol_column
 
-print(canada_1986)
-
-
-canada_alcohol = canada_1986[:,4]
-
-print(canada_alcohol)
-
-empty_strings = canada_alcohol == ''
-canada_alcohol[empty_strings] = '0'
-
-canada_alcohol = canada_alcohol.astype(float)
-
-print(canada_alcohol)
-
-total_canadian_drinking = canada_alcohol.sum()
-print(total_canadian_drinking)
+print(totals)
