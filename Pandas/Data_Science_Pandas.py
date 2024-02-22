5da1dc1034423d05ec7e41dd740bd99bb6d4f808
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-22 13:17:20 +0100

    selectione de lignes : DONE

--- Pandas/Data_Science_Pandas.py
+++ Pandas/Data_Science_Pandas.py
@@ -2,16 +2,29 @@ import pandas as pd
 
 food_info = pd.read_csv("food_info.csv")
 
-row_100 = food_info.loc[99]
+# Dataframe contenant les lignes d'index 3, 4, 5 et 6 
 
-print(row_100)
+food_info.loc[3:6]
 
-# le type Objet représente une chaine de caractères
-# le type Int64 représente un entier
-# le type Float64 représente un décimal
-# le type datetime64 représente une date
-# le type bool représente une valeur booléenne
+# ici on a utilisé la méthode loc[] pour sélectionner les lignes 3 à 6 de food_info.
 
-print(food_info.dtypes)
+# print(food_info.loc[3:6])
 
-#ici on affiche le type de chaque colonne
+# Dataframe contenant les lignes d'index 2, 5 et 10.
+
+# méthode 1
+two_five_ten = [2,5,10]
+food_info.loc[two_five_ten]
+# print(food_info.loc[two_five_ten])
+
+#méthode 2 
+
+food_info.loc[[2,5,10]]
+# print(food_info.loc[[2,5,10]])
+
+num_rows = food_info.shape[0]
+last_rows = food_info.loc[num_rows-5:num_rows-1]
+
+# ici on a utilisé la méthode shape et la méthode loc[] pour sélectionner les 5 dernières lignes de food_info.
+
+print(last_rows)
