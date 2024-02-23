d21eaabf401590a2c299374367f4b5cf400af9cf
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-23 12:11:23 +0100

    opération mathématique entre colones : DONE

--- Pandas/Data_Science_Pandas.py
+++ Pandas/Data_Science_Pandas.py
@@ -2,14 +2,17 @@ import pandas as pd
 
 food_info = pd.read_csv("food_info.csv")
 
-# score = 2 x protein - 0.75 x lipide 
+water_energy = food_info["Water_(g)"] * food_info["Energ_Kcal"]
+print(water_energy[0:5])
 
-col_names = food_info.columns.tolist()
+# ici on multiplie les valeurs de la colonne "Water_(g)" par les valeurs de la colonne "Energ_Kcal" pour chaque ligne
+# on obtient un objet Series avec les valeurs résultantes
 
-sodium_grams = food_info["Sodium_(mg)"] / 1000
 
-sugar_milligrams = food_info["Sugar_Tot_(g)"] * 1000
+grams_of_protein_per_gram_of_water = food_info["Protein_(g)"] / food_info["Water_(g)"]
+milligrams_of_calcium_and_iron = food_info["Calcium_(mg)"] + food_info["Iron_(mg)"]
 
-print(sodium_grams)
-print(sugar_milligrams)
+print(grams_of_protein_per_gram_of_water)
+print("**delimiter**")
+print(milligrams_of_calcium_and_iron)
 
