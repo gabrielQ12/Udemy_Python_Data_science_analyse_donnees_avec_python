5d4032553a27372b327a243b5eb1595008a070c5
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-23 12:14:27 +0100

    indice nut : DONE

--- Pandas/Data_Science_Pandas.py
+++ Pandas/Data_Science_Pandas.py
@@ -2,17 +2,9 @@ import pandas as pd
 
 food_info = pd.read_csv("food_info.csv")
 
-water_energy = food_info["Water_(g)"] * food_info["Energ_Kcal"]
-print(water_energy[0:5])
+protein = food_info["Protein_(g)"] *2
+fat = food_info["Lipid_Tot_(g)"] * -0.75
 
-# ici on multiplie les valeurs de la colonne "Water_(g)" par les valeurs de la colonne "Energ_Kcal" pour chaque ligne
-# on obtient un objet Series avec les valeurs résultantes
+rating = protein + fat
 
-
-grams_of_protein_per_gram_of_water = food_info["Protein_(g)"] / food_info["Water_(g)"]
-milligrams_of_calcium_and_iron = food_info["Calcium_(mg)"] + food_info["Iron_(mg)"]
-
-print(grams_of_protein_per_gram_of_water)
-print("**delimiter**")
-print(milligrams_of_calcium_and_iron)
-
+print(rating)
--- Pandas/cours_pandas.md
+++ Pandas/cours_pandas.md
@@ -256,6 +256,24 @@ print(sugar_milligrams)
 ------
 ```Python
 
+import pandas as pd
+
+food_info = pd.read_csv("food_info.csv")
+
+water_energy = food_info["Water_(g)"] * food_info["Energ_Kcal"]
+print(water_energy[0:5])
+
+# ici on multiplie les valeurs de la colonne "Water_(g)" par les valeurs de la colonne "Energ_Kcal" pour chaque ligne
+# on obtient un objet Series avec les valeurs résultantes
+
+
+grams_of_protein_per_gram_of_water = food_info["Protein_(g)"] / food_info["Water_(g)"]
+milligrams_of_calcium_and_iron = food_info["Calcium_(mg)"] + food_info["Iron_(mg)"]
+
+print(grams_of_protein_per_gram_of_water)
+print("**delimiter**")
+print(milligrams_of_calcium_and_iron)
+
 ```
 ------
 ```Python
